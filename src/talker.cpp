fad8463b28f451eb2a365e7d259507d1db60d5d1
zzimits <zachary@zimits.com>
2018-10-27 20:19:49 -0400

    finished the final tutorial: Examining Publisher Subscriber. I edited publisher to publish a custom string.

--- src/talker.cpp
+++ src/talker.cpp
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "Zach Zimits has completed the tutorials!" << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
