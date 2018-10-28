b81729a15bee252a371a48d873baca43b48ed2ce
zzimits <zachary@zimits.com>
2018-10-27 20:54:55 -0400

    fixed issues with cpplint and cppcheck and saved output to indivdual text files

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,13 +1,14 @@
+Copyright 2018 Zachary Zimits
+
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-#include <sstream>
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -53,8 +54,7 @@ int main(int argc, char **argv)
    * a unique string for each message.
    */
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
